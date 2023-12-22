b364a216deca60fb70dbcc0cfa9b7cccb605d38b
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:16:31 +0100

    Add files via upload

---
+++ depozit/src/depozit/Kamata.java
@@ -0,0 +1,24 @@
+package depozit;
+
+import java.util.Scanner;
+
+public class Kamata {
+
+	public static void main(String[] args) {
+	
+	double depozit= "kamata,iznos ";
+	Scanner tastatura=new Scanner (System.in);
+	System.out.println ("vnesi poceten depozit");
+	depozit=tastatura.nextDouble();
+	System.out.println("vnesi godisna kamatna stapka");
+	kamata =tastatura.nextDouble();
+	
+			
+			
+	
+	
+	
+		
+	}
+
+}
